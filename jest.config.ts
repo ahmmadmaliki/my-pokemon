08bcdee3d87df7edb6d7e2b48e6b475c01e198b0
chornos13 <chornos13@hotmail.com>
2021-02-16 18:15:19 +0700

    refactor: remove unused code

--- jest.config.ts
+++ jest.config.ts
@@ -16,14 +16,4 @@ export default async (): Promise<any> => {
       '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     },
   }
-
-  return {
-    moduleDirectories: ['node_modules', 'src'],
-    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-    setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    },
-  }
 }
