6b37ec7fe83d837c1c76293fa7821d17a5cf8514
chornos13 <chornos13@hotmail.com>
2021-02-17 11:29:02 +0700

    feat: implement feature My Pokemon

--- src/views/MyPokemon/MyPokemon.tsx
+++ src/views/MyPokemon/MyPokemon.tsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Content from 'components/Content/Content'
 import { useHeader } from 'layouts/containers/Public/Header/Header'
+import ContextContainer from 'layouts/containers/Public/ContextContainer'
+import { isEmpty } from 'lodash'
+import { Result, Button } from 'antd'
+import Link from 'next/link'
+import ListPokemon from 'components/ListPokemon/ListPokemon'
 
 function MyPokemon() {
   useHeader({
     title: 'My Pokemon',
   })
 
-  return <Content>My Pokemon</Content>
+  const { storagePokemon } = useContext(ContextContainer)
+
+  if (isEmpty(storagePokemon.data)) {
+    return (
+      <div data-testid={'emptyView'}>
+        <Result
+          status={404}
+          title={'No pokemon here :('}
+          subTitle={
+            <Link href={'/'}>
+              <a>
+                <Button type={'primary'}>Go Catch Them</Button>
+              </a>
+            </Link>
+          }
+        />
+      </div>
+    )
+  }
+
+  return (
+    <Content>
+      <ListPokemon dataSource={storagePokemon.data} />
+    </Content>
+  )
 }
 
 export default MyPokemon
