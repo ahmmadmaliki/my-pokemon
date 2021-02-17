9f5edb6d6228fd874686ca609dc4858acadfeebf
chornos13 <chornos13@hotmail.com>
2021-02-17 11:31:38 +0700

    refactor: use ListPokemon component

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,11 +2,9 @@ import React, { useMemo } from 'react'
 import Content from 'components/Content/Content'
 import { useHeader } from 'layouts/containers/Public/Header/Header'
 import { List, Button, notification } from 'antd'
-import useValueBreakpoint from 'hooks/useValueBreakpoint'
-import Link from 'next/link'
 import PokemonCard from 'components/PokemonCard'
 import usePokemons from 'data/usePokemons'
-import { useWindowWidth } from '@react-hook/window-size'
+import ListPokemon from 'components/ListPokemon/ListPokemon'
 
 const TOTAL_FETCH = 15
 
@@ -15,17 +13,7 @@ function Home() {
     title: 'Pokemon',
   })
 
-  const width = useWindowWidth()
-
   const { fetchMore, data, variables, loading, error } = usePokemons({})
-
-  const breakpointColumn = useValueBreakpoint({
-    xs: width >= 428 ? 2 : 1,
-    sm: 2,
-    md: 3,
-    lg: 4,
-    xl: 5,
-  })
 
   const elListLoading = useMemo(() => {
     return Array(15).fill(
@@ -47,12 +35,8 @@ function Home() {
           paddingBottom: 50,
         }}
       >
-        <List
-          grid={{
-            gutter: [20, 20] as any,
-            column: breakpointColumn.value,
-          }}
-          itemLayout="horizontal"
+        <ListPokemon
+          itemLoading={elListLoading}
           loadMore={
             data &&
             data?.pokemons?.next !== null && (
@@ -83,25 +67,6 @@ function Home() {
             )
           }
           dataSource={[...(data?.pokemons?.results || []), loading]}
-          renderItem={(item) => {
-            if (!item) {
-              return <React.Fragment />
-            }
-
-            if (item === true) {
-              return elListLoading
-            }
-
-            return (
-              <List.Item data-testid={'itemPokemon'}>
-                <Link href={`/detail?name=${item.name}`}>
-                  <a>
-                    <PokemonCard name={item.name} image={item.image} />
-                  </a>
-                </Link>
-              </List.Item>
-            )
-          }}
         />
       </Content>
     </React.Fragment>
